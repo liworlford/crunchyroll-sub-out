f9753d179742e51a0ca85a72c57dee9ec5867970
Kohei Tsuchiya <doyahirohira@gmail.com>
2024-09-24 16:18:25 +0900

    TypeScript化

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+window.addEventListener("load", async () => {
+  if (location.host === "www.crunchyroll.com") {
+    console.log(`[${location.host}] Crunchyroll SubOut started.`);
+
+    const observer = new MutationObserver((_, observer) => {
+      const wrapper = document.querySelector(".video-player-wrapper");
+      if (wrapper) {
+        wrapper.insertAdjacentHTML(
+          "afterend",
+          '<div id="crunchyroll-sub-out"></div>'
+        );
+        observer.disconnect();
+        console.log(`[${location.host}] Observer disconnected.`);
+      }
+    });
+
+    const contentEl = document.querySelector("#content");
+    if (contentEl) {
+      observer.observe(contentEl, {
+        childList: true,
+        subtree: true,
+      });
+    }
+
+    window.addEventListener("message", (e) => {
+      if (e.origin !== "https://static.crunchyroll.com") return;
+
+      if (typeof e.data === "object" && e.data.type === "subtitle") {
+        const targetEl = document.querySelector("#crunchyroll-sub-out");
+        if (targetEl) (targetEl as HTMLElement).innerText = e.data.subtitle;
+      }
+    });
+  }
+
+  if (location.host === "static.crunchyroll.com") {
+    console.log(`[${location.host}] Crunchyroll SubOut started.`);
+
+    const targetEl = document.querySelector("#vilosVttJs");
+    let exSubtitle = "";
+
+    if (targetEl) {
+      const observer = new MutationObserver(() => {
+        if (!(targetEl as HTMLElement).innerText) return;
+
+        if ((targetEl as HTMLElement).innerText !== exSubtitle) {
+          window.parent.postMessage(
+            { type: "subtitle", subtitle: (targetEl as HTMLElement).innerText },
+            "https://www.crunchyroll.com/watch/*"
+          );
+          exSubtitle = (targetEl as HTMLElement).innerText;
+        }
+      });
+
+      observer.observe(targetEl, { childList: true, subtree: true });
+    }
+  }
+});
